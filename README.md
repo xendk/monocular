2a7d251801243e2dbfe767f75845044f439add32
Thomas Fini Hansen <xen@xen.dk>
2015-02-14 23:38:26 +0100

    First version.

---
+++ README.md
@@ -0,0 +1,68 @@
+
+Monocular
+=========
+
+Monocular is a simple command line interface to submit test images to
+Applitools Eyes. It allows for submitting images captured in any test
+environment, as long as the evironment is able to start a process and
+feed it Line Delimited JSON over STDIN or a file.
+
+Using STDIN is preferred, as it allows Monocular to start uploading
+images as soon as possible.
+
+Usage
+-----
+
+    Usage: node monocular.js [file] [options]
+
+    file     File to read
+
+    Options:
+       -d, --delete     Delete images after processing
+       -n, --simulate   Simulate submission to Applitools
+
+
+JSON data
+---------
+
+Each object should contain a command key that defines the command.
+
+Commands:
+
+'init':
+    Set basic parameters. Expects:
+        'apiKey': Your Applitools Eyes API key.
+        'os': The OS running the test.
+        'browser': The browser tests are being run on (or "hosting app").
+
+'open':
+    Opens a test session. Expects:
+        'appName': Name of the application.
+        'testName': Name of the test.
+
+'image':
+    Submit an image. Expects:
+        'file': PNG file to read. Will be deleted if the -d switch is
+            provided.
+        'tag': Image tag.
+
+'end':
+    Ends the session, no arguments expected.
+        
+
+Output and return code
+----------------------
+
+On successful submission, Monocular prints the result of the test to
+STDOUT like this:
+
+Result: OK https://eyes.applitools.com/app/sessions/251978348342540
+
+or:
+
+Result: FAIL https://eyes.applitools.com/app/sessions/251978347866624
+
+The URL is the URL of the test session for inspection at Applitools
+site. The exit code of Monocular is 0 for passing tests and 20 for
+failing tests (to not collide with internal nodejs codes). All other
+exit codes denote an error.
---
+++ monocular.js
@@ -0,0 +1,119 @@
+
+var split2 = require('split');
+var fs = require('fs');
+var Eyes = require('eyes.images').Eyes;
+
+var package = require('./package.json');
+
+var opts = require('nomnom')
+  .options(
+    {
+      file: {
+        position: 0,
+        help: 'File to read',
+        required: false
+      },
+      delete: {
+        abbr: 'd',
+        flag: true,
+        help: 'Delete images after processing'
+      },
+      simulate: {
+        abbr: 'n',
+        flag: true,
+        help: 'Simulate submission to Applitools'
+      }
+    }
+  )
+  .parse();
+
+process.stdin.setEncoding('utf8');
+
+var input = process.stdin;
+
+if (opts.file) {
+  input = fs.createReadStream(opts.file);
+}
+
+var eyes = new Eyes(false, opts.simulate ? true : false);
+var eyesPromise;
+
+var handleMessage = function(data) {
+  if (typeof data.cmd === 'undefined') {
+    // do nothing.
+    return;
+  }
+
+  switch (data.cmd) {
+  case 'init':
+    var apiKey = data.apiKey || '';
+    eyes.setApiKey(apiKey);
+    var os = data.os || '';
+    eyes.setOs(os);
+    var browser = data.browser || '';
+    eyes.setHostingApp(browser);
+    break;
+
+  case 'open':
+    var appName = data.appName || '';
+    var testName = data.testName || '';
+    // We're not implementing imageSize.
+    eyesPromise = eyes.open(appName, testName, {width: 800, height: 600});
+    break;
+
+  case 'image':
+    var file = data.file || '';
+    var tag = data.tag || '';
+    eyesPromise = eyesPromise.then(function () {
+      var imageData = fs.readFileSync(file);
+      return eyes.checkImage(imageData, tag);
+    });
+    if (opts.delete) {
+      eyesPromise = eyesPromise.then(function () {
+        fs.unlinkSync(file);
+      });
+    }
+    break;
+    
+  case 'end':
+    eyesPromise.then(function () {
+      return eyes.close(false);
+    },function () {
+        return eyes.abortIfNotClosed();
+    })
+    .then(function (results) {
+      console.log('Result: ' + (results.isPassed ? 'OK' : 'FAIL') + ' ' + results.url);
+      if (!results.isPassed) {
+        process.exit(20);
+      }
+    })
+      .catch(function (err) {console.log('Error: ' + err);});
+    break;
+  }
+};
+
+input.pipe(split2(JSON.parse))
+  .on('data', function (obj) {
+    //each chunk now is a a js object
+    try {
+      handleMessage(obj);
+    }
+    catch (e) {
+      console.log(e.message);
+      eyes.abortIfNotClosed(function () {
+        process.exit(1);
+      });
+    }
+  })
+  //.on('close', function () { console.log('close');})
+  .on('error', function (err) {
+    // We'll just quietly gobble these up.
+    if (err.message !== 'Unexpected end of input') {
+      throw err;
+    }
+  });
+
+
+// process.stdin.on('end', function() {
+//   process.stdout.writenl('end');
+// });
